22a954859a5f6b86137414cdb3779425838b2289
mahreen <mahreenaisha.work@gmail.com>
2025-10-24 19:08:36 +0530

    test 6

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ provider "google" {
 
 //creating storage bucket
 resource "google_storage_bucket" "my-bucket" {
-  name          = "vit-project-githubdemo-bucket"
-  location      = "US"
-  force_destroy = true
+  name                     = "vit-project-githubdemo-bucket"
+  location                 = "US"
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
 
