9f1a040bc32c882587ce9152182500e293a7cbef
mahreen <mahreenaisha.work@gmail.com>
2025-10-24 19:07:36 +0530

    test 5

--- main.tf
+++ main.tf
@@ -19,6 +19,13 @@ resource "google_storage_bucket" "my-bucket" {
   name          = "vit-project-githubdemo-bucket"
   location      = "US"
   force_destroy = true
+  public_access_prevention = "enforced"
+}
 
+//creating second bucket
+resource "google_storage_bucket" "my-second-bucket" {
+  name                     = "vit-project-githubdemo-bucket-2"
+  location                 = "US"
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
