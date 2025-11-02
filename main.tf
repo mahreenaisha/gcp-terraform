fc870389ea0fa01e2567a1212268de109bb4da29
mahreen <mahreenaisha.work@gmail.com>
2025-11-02 12:24:02 +0530

    create vm

--- main.tf
+++ main.tf
@@ -19,11 +19,32 @@ provider "google" {
   region  = "us-central1"
 }
 
-# //creating storage bucket
-# resource "google_storage_bucket" "my-bucket" {
-#   name                     = "vit-project-githubdemo-bucket"
-#   location                 = "US"
-#   force_destroy            = true
-#   public_access_prevention = "enforced"
-# }
+//creating storage bucket
+resource "google_storage_bucket" "my-bucket" {
+  name                     = "vit-project-githubdemo-bucket"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
 
+resource "google_compute_instance" "vm-from-terraform"{
+  name = "vm-from-terraform"
+
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+}
