53273ee181d87633368e2e76cb6a73e97505f935
mahreen <mahreenaisha.work@gmail.com>
2025-11-02 10:46:13 +0530

    removed bucket code

--- main.tf
+++ main.tf
@@ -14,11 +14,5 @@ provider "google" {
 }
 
 
-//creating storage bucket
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "vit-project-githubdemo-bucket"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
-}
+
 
