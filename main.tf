3454838397a3aefa9903948d2f2857b4ba810b1d
mahreen <mahreenaisha.work@gmail.com>
2025-11-07 16:27:11 +0530

    Add my-vm-79-1

--- main.tf
+++ main.tf
@@ -27,10 +27,17 @@ resource "google_storage_bucket" "my-bucket-79-1" {
   public_access_prevention = "enforced"
 }
 
+resource "google_compute_instance" "my-vm-79-1" {
+  name         = "vm-79-1"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
 
-
-
-
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
     }
   }
 
