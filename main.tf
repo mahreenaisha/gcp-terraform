5074df56b2a0e25f0a8fb2e7e60f1aed62f5e1c8
mahreen <mahreenaisha.work@gmail.com>
2025-11-07 18:43:11 +0530

    Delete my-bucket-79-2

--- main.tf
+++ main.tf
@@ -29,9 +29,3 @@ resource "google_storage_bucket" "my-bucket-79-1" {
 
 
 
-resource "google_storage_bucket" "my-bucket-79-2" {
-  name                     = "storage-bucket-79-2"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
-}
