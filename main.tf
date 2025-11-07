5cce059562eeb50768204203ec85b11c9f1c3f21
mahreen <mahreenaisha.work@gmail.com>
2025-11-07 16:48:56 +0530

    Add my-bucket-79-2

--- main.tf
+++ main.tf
@@ -31,3 +31,11 @@ resource "google_storage_bucket" "my-bucket-79-1" {
 
 
 
+
+
+resource "google_storage_bucket" "my-bucket-79-2" {
+  name                     = "storage-bucket-79-2"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
